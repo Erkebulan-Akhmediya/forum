bb7215822a128e7fb67b5de5e7160c827fc03209
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-05-01 17:50:26 +0500

    added utils essential to controllers

---
+++ utils/method_handler.go
@@ -0,0 +1,13 @@
+package utils
+
+import "net/http"
+
+type MethodHandler map[string]http.Handler
+
+func (mh MethodHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if handler, ok := mh[r.Method]; ok {
+		handler.ServeHTTP(w, r)
+	} else {
+		SendMessage(w, "Method not found", http.StatusMethodNotAllowed)
+	}
+}
---
+++ utils/send_message.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+type message struct {
+	Message string `json:"message"`
+}
+
+func SendMessage(w http.ResponseWriter, msg string, code int) {
+	w.WriteHeader(code)
+	m := message{Message: msg}
+	if err := json.NewEncoder(w).Encode(m); err != nil {
+		log.Printf("Error sending message: %s", err)
+	}
+}
