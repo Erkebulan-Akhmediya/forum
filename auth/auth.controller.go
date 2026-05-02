474656b3349221c63e336b1c70cb22d1cdc4745c
Erkebulan-Akhmediya <yerkebulan20030921@gmail.com>
2026-05-02 12:46:49 +0500

    added password hashing

--- auth/auth.controller.go
+++ auth/auth.controller.go
@@ -6,6 +6,8 @@ import (
 	"forum/utils"
 	"log"
 	"net/http"
+
+	"golang.org/x/crypto/bcrypt"
 )
 
 type signUpHandler struct {
@@ -26,7 +28,23 @@ func (h *signUpHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		utils.SendMessage(w, "Error parsing request", 400)
 		return
 	}
-	if err := h.service.Create(dto.Username, dto.Password, dto.Email); err != nil {
+
+	pwdBytes := []byte(dto.Password)
+	// this check is necessary because bcrypt.GenerateFromPassword
+	// does not operate on byte arrays longer that 72
+	if len(pwdBytes) > 72 {
+		utils.SendMessage(w, "Password too long", 400)
+		return
+	}
+
+	pwdBytes, err := bcrypt.GenerateFromPassword(pwdBytes, bcrypt.DefaultCost)
+	if err != nil {
+		log.Println("Error hashing password:", err)
+		utils.SendMessage(w, "Failed to hash password", 500)
+		return
+	}
+
+	if err := h.service.Create(dto.Username, string(pwdBytes), dto.Email); err != nil {
 		log.Println("Error creating new user:", err)
 		utils.SendMessage(w, "Sign up failed", 500)
 		return
--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.26.2
 require github.com/mattn/go-sqlite3 v1.14.44
 
 require github.com/joho/godotenv v1.5.1
+
+require golang.org/x/crypto v0.50.0
